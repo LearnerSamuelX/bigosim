86bd8ad4143c987464a1f4f11b42e805fde102a0
SamuelX <samxiezx1115@gmail.com>
2020-08-07 17:35:26 -0400

    chart work

--- src/components/chart/Barchart.js
+++ src/components/chart/Barchart.js
@@ -1,26 +1,39 @@
 import React,{useEffect,useRef,useState} from 'react';
+import "./Barchart.css"
 import * as d3 from 'd3'
 
 const Barchart = () => {
-    const [array,setArray]=useState([9,8,7,6,5,4])
+    const [array,setArray]=useState([12,24,32])
     const d3Container = useRef(null);
 
     useEffect(()=>{
-        const svg = d3.select(d3Container.current)
-        d3.select(d3Container.current).selectAll("rect").data(array).enter().append("rect")
-                .attr('height',(d,i)=>d)
-                .attr('width',25)
-                .attr("fill","blue")
+        const svg = d3.select(d3Container.current).append('svg').attr('width','100%').attr('height','300')
+        svg.selectAll("rect").data(array).enter().append("rect")
+        .attr("height",(d,i)=>d*3)
+        .attr("width","15")
+        .attr("fill","blue")
+        .attr('x',(d,i)=>200+20*i)
+        .attr('y',(d,i)=>100-d*3)
 
 
+            // .attr("height",(d,i)=>{return d*3})
+            // .attr("width","25")
+            // .attr("fill","blue")
+            // .attr("x",(d,i)=> 30*i)
+            // .attr("y",(d,i)=>{return 100-d*3})
+                
+        // svg.selectAll('text').data(array).enter().append("text")
+        //         .attr('x',(d,i)=>150+i*15)
+        //         .attr('y',(d,i)=>height+100-5-d*10)
+        //         .text((d)=>d)
+        
     })
-
 
 
     return(
         <div id='chart-container'>
             <h3>Bar Chart</h3>
-            <svg className="d3-component"width={500} height={400}ref={d3Container}></svg>
+            <div className="d3-component"ref={d3Container}></div>
         </div>
     )
 }
