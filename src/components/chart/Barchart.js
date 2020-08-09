29b715167262fa778dcaeeced62640a261b1319d
SamuelX <samxiezx1115@gmail.com>
2020-08-09 11:29:17 -0400

    axis completed

--- src/components/chart/Barchart.js
+++ src/components/chart/Barchart.js
@@ -8,26 +8,32 @@ const Barchart = () => {
 
     useEffect(()=>{
         const height = 100 //height of the bar
+        const width = 200
 
-        const svg = d3.select(d3Container.current).append('svg').attr('width','100%').attr('height','300')
+        const svg = d3.select(d3Container.current).append('svg').attr('width','400').attr('height','200')
         
+        // var x = d3.scaleTime().domain([]).range([0,width])
         var y = d3.scaleLinear().domain([0,40]).range([height,0])
 
-        var yAxis = d3.axisLeft(y)
+        var yAxis = d3.axisLeft(y).ticks(5)
 
-        var chartGroup = svg.append('g').attr('transform','translate(0,0)')
+        var chartGroup = svg.append('g').attr('transform','translate(100,50)')
         
         chartGroup.selectAll("rect").data(array).enter().append("rect")
                 .attr("height",(d,i)=>d*3)
                 .attr("width","15")
                 .attr("fill","blue")
-                .attr('x',(d,i)=>500+25*i)
-                .attr('y',(d,i)=>height-d*3+77)
+                .attr('x',(d,i)=>25*i)
+                .attr('y',(d,i)=>height-d*3)
                 
         chartGroup.selectAll('text').data(array).enter().append("text")
-                .attr('x',(d,i)=>500+25*i)
-                .attr('y',(d,i)=>height-5-d*3+75)
+                .attr('x',(d,i)=>25*i)
+                .attr('y',(d,i)=>height-5-d*3+2)
                 .text((d,i)=>d)
+        
+        chartGroup.append('g').attr('class','axis y')
+                // .attr('transform','translate(500,76)')
+                .call(yAxis)
         
     })
 
