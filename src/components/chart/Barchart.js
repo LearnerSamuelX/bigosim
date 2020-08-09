15b6c18a6913e59047dfadfbb74328060fdd5635
SamuelX <samxiezx1115@gmail.com>
2020-08-08 22:20:09 -0400

    categorized

--- src/components/chart/Barchart.js
+++ src/components/chart/Barchart.js
@@ -3,29 +3,31 @@ import "./Barchart.css"
 import * as d3 from 'd3'
 
 const Barchart = () => {
-    const [array,setArray]=useState([12,24,32])
-    const d3Container = useRef(null);
+    const [array,setArray]=useState([23,24,32,5,6,7,8,12])
+    const d3Container = useRef(null);   
 
     useEffect(()=>{
+        const height = 100 //height of the bar
+
         const svg = d3.select(d3Container.current).append('svg').attr('width','100%').attr('height','300')
-        svg.selectAll("rect").data(array).enter().append("rect")
-        .attr("height",(d,i)=>d*3)
-        .attr("width","15")
-        .attr("fill","blue")
-        .attr('x',(d,i)=>200+20*i)
-        .attr('y',(d,i)=>100-d*3)
+        
+        var y = d3.scaleLinear().domain([0,40]).range([height,0])
 
+        var yAxis = d3.axisLeft(y)
 
-            // .attr("height",(d,i)=>{return d*3})
-            // .attr("width","25")
-            // .attr("fill","blue")
-            // .attr("x",(d,i)=> 30*i)
-            // .attr("y",(d,i)=>{return 100-d*3})
+        var chartGroup = svg.append('g').attr('transform','translate(0,0)')
+        
+        chartGroup.selectAll("rect").data(array).enter().append("rect")
+                .attr("height",(d,i)=>d*3)
+                .attr("width","15")
+                .attr("fill","blue")
+                .attr('x',(d,i)=>500+25*i)
+                .attr('y',(d,i)=>height-d*3+77)
                 
-        // svg.selectAll('text').data(array).enter().append("text")
-        //         .attr('x',(d,i)=>150+i*15)
-        //         .attr('y',(d,i)=>height+100-5-d*10)
-        //         .text((d)=>d)
+        chartGroup.selectAll('text').data(array).enter().append("text")
+                .attr('x',(d,i)=>500+25*i)
+                .attr('y',(d,i)=>height-5-d*3+75)
+                .text((d,i)=>d)
         
     })
 
