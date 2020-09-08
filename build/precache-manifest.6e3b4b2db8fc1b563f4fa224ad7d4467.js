0a511692a27463f4f5ca774c50351e98127dfbb5
SamuelX <samxiezx1115@gmail.com>
2020-09-08 14:42:47 -0400

    logo and title

---
+++ build/precache-manifest.6e3b4b2db8fc1b563f4fa224ad7d4467.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6693ade0c778a2e79274c1123db4d6b0",
+    "url": "/bigosim/index.html"
+  },
+  {
+    "revision": "c4d3b326b90645a1c0c9",
+    "url": "/bigosim/static/css/main.22f74b79.chunk.css"
+  },
+  {
+    "revision": "cdd6c4293db3238ff207",
+    "url": "/bigosim/static/js/2.5c5b9a7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bigosim/static/js/2.5c5b9a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4d3b326b90645a1c0c9",
+    "url": "/bigosim/static/js/main.0b95c586.chunk.js"
+  },
+  {
+    "revision": "4d9f065c462eb1e8ef87",
+    "url": "/bigosim/static/js/runtime-main.4d2e6771.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bigosim/static/media/logo.5d5d9eef.svg"
+  }
+]);
